3e958a6e7a2d765c70e7850cf58a5bd35d70982f
minseong7164 <minseong7164@naver.com>
2025-04-25 17:16:08 +0900

    feat: 20250425 / constructor,setter,getter,modifier

--- src/main/java/ch09_classes/Car.java
+++ src/main/java/ch09_classes/Car.java
@@ -23,15 +23,22 @@ public class Car {
      */
     int speed;
     String color;
+    // 생성자를 배우면 해결되는 개념
+//    String eval2 = (speed > 100 ? "빠릅니다." : "그냥저냥입니다." );
+
     public void dirve(){
         System.out.println(color + "자동차가 주행 중입니다.\n최고 속도는 " + speed + "입니다.");
     }
+
     public String brake(){
         return color + "자동차가 멈췄습니다.";
     }
+
     public void displayInfo(){
         String eval = (speed > 100 ? "빠릅니다." : "그냥저냥입니다." );
         System.out.println("이차의 색깔은 " + color + "이고, ");
         System.out.println("최고 속도는 " + speed + "km/h입니다." + eval);
     }
+
+    // Constructor / ConstructorMain -> main
 }
